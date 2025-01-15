7e5a409a7492dc32e1c950957e4b90149e25dbe8
Aidan Y <jamesayipt6@gmail.com>
2025-01-15 14:41:53 -0800

    Updated form and style

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 // css
+import './styles/normalize_v2.css'
 import './styles/index.css'
 import './styles/home.css'
 import './styles/music_icons.css'
 import './styles/artists.css'
 import './styles/sample_packs.css'
 import './styles/about.css'
+import './styles/submit.css'
 // components
 import App from './App.jsx'
 
