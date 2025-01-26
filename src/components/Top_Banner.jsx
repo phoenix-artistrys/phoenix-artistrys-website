446df08da66670c60a8dc6e4519b366469e177b4
Aidan Y <jamesayipt6@gmail.com>
2025-01-26 00:36:07 -0800

    Play video inline attribute added

--- src/components/Top_Banner.jsx
+++ src/components/Top_Banner.jsx
@@ -1,5 +1,15 @@
 function Top_Banner({ Banner_Video }) {
-  return <video src={Banner_Video} autoPlay muted loop className="video_banner"></video>;
+  return (
+    <video
+      src={Banner_Video}
+      autoPlay
+      webkit-playsinline
+      playsInline
+      muted
+      loop
+      className="video_banner"
+    ></video>
+  );
 }
 
 export default Top_Banner;
