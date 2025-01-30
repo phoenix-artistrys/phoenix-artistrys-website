bf8feb444a501896a532a74e6ab3dce24b560b3d
Aidan Y <jamesayipt6@gmail.com>
2025-01-29 22:50:56 -0800

    created artwork.jsx and .css modified about.css to slim line breaker

--- src/components/Artwork.jsx
+++ src/components/Artwork.jsx
@@ -1,9 +1,13 @@
 function Artwork({ phoenix_logo_large }) {
-    return (
-        <div className="">
-            <img src={phoenix_logo_large} alt="Phoenix Artistrys Logo" />
-        </div>
-    );
+  return (
+    <div className="flex_row">
+      <img
+        src={phoenix_logo_large}
+        alt="Phoenix Artistrys Logo"
+        className="phoenix_logo_large"
+      />
+    </div>
+  );
 }
 
 export default Artwork;
