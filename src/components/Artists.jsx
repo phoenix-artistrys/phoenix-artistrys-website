b5289f05af81c66984ff02a76a6080a4540a22ef
Aidan Y <jamesayipt6@gmail.com>
2025-02-15 22:22:41 -0800

    Final code for 1.0 v1

--- src/components/Artists.jsx
+++ src/components/Artists.jsx
@@ -1,8 +1,8 @@
 function Artists() {
   return (
-    <div className="block_background_white">
-      <h2 className="header_dark">Artists</h2>
-      <a href="https://sites.google.com/view/phoenixartistrys-artists/artists" className="hero_link">
+    <div className="block_background_white block_background_dark">
+      <h2 className="">Artists</h2>
+      <a href="https://sites.google.com/view/phoenixartistrys-artists/artists" className="hero_link blue_button_link">
         Discover
       </a>
     </div>
