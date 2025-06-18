348e909f247ee3e6e449f9aeaf30dc3669311e7f
Aidan Y <jamesayipt6@gmail.com>
2025-06-17 23:28:31 -0700

    trial design for new look

--- src/components/Top_Banner.jsx
+++ src/components/Top_Banner.jsx
@@ -1,4 +1,22 @@
+import React, { useEffect } from "react";
+
 function Top_Banner({ Banner_Video }) {
+  // Pause the banner video when scrolling down, play when at top
+  useEffect(() => {
+    const handleScroll = () => {
+      const video = document.getElementsByClassName("video_banner")[0];
+      if (video) {
+        if (window.scrollY > 500) {
+          video.pause();
+        } else {
+          video.play();
+        }
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <video
       src={Banner_Video}
